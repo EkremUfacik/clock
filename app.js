d18fa757cbe2a7e7fe9b6cb1856c63dd628e8fad
ekrem <ekremufacik07@gmail.com>
2022-12-02 16:33:10 +0300

    ee

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-// console.log(year);
+setInterval(time, 1000);
 
-setInterval(() => {
+function time() {
   let seconds = new Date().getSeconds();
   let minutes = new Date().getMinutes();
   let hours = new Date().getHours();
@@ -9,7 +9,7 @@ setInterval(() => {
   let year = new Date().getFullYear();
   let day = new Date().toLocaleString("en-tr", { weekday: "long" });
   let session = "AM";
-  //   console.log(day);
+
   const clock = document.getElementById("clock");
   const calendar = document.getElementById("calendar");
   const currentDay = document.getElementById("current-day");
@@ -18,23 +18,13 @@ setInterval(() => {
     hours = hours - 12;
     session = "PM";
   }
-  if (String(hours).length == 1) {
-    hours = "0" + hours;
-  }
-  if (String(minutes).length == 1) {
-    minutes = "0" + minutes;
-  }
-  if (String(seconds).length == 1) {
-    seconds = "0" + seconds;
-  }
-  if (String(date).length == 1) {
-    date = "0" + date;
-  }
-  if (String(month).length == 1) {
-    month = "0" + month;
-  }
+  if (hours < 10) hours = "0" + hours;
+  if (minutes < 10) minutes = "0" + minutes;
+  if (seconds < 10) seconds = "0" + seconds;
+  if (date < 10) date = "0" + date;
+  if (month < 10) month = "0" + month;
 
   clock.innerText = hours + ":" + minutes + ":" + seconds + " " + session;
   calendar.innerText = date + "/" + month + "/" + year;
   currentDay.innerText = day;
-}, 1000);
+}
